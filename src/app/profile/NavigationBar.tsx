ebec7214004541122ed8283ceb2e87545ffe47eb
alextrnphn <107496475+alextrnphn@users.noreply.github.com>
2024-05-14 11:14:37 -0700

    Set up and add on icon file for nagivation bar

---
+++ src/app/profile/NavigationBar.tsx
@@ -0,0 +1,12 @@
+interface NavigationBarProps {}
+import { Icon } from "../profile/page";
+
+interface NavigationBarProps {
+  items: Icon[];
+}
+export default function NavigationBar({ items }: NavigationBarProps) {
+  // items.map((icon) => {
+  //   console.log(icon.altText);
+  // });
+  return <div>{/* //loop button image */}</div>;
+}
