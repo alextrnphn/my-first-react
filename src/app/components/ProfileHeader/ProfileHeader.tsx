1d131422f31a1b2a4af23df8f2396144f8bf279b
alextrnphn <107496475+alextrnphn@users.noreply.github.com>
2024-05-20 16:34:48 -0700

    fixed - add mobile style

--- src/app/components/ProfileHeader/ProfileHeader.tsx
+++ src/app/components/ProfileHeader/ProfileHeader.tsx
@@ -1,4 +1,41 @@
+import styled from "styled-components";
+
 import { useState, useEffect } from "react";
+import Image from "next/image";
+
+const ProfileHeaderContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+`;
+const ProfileImage = styled(Image)`
+  border-radius: 50%;
+  margin: auto auto;
+  @media (min-width: 480px) {
+    height: 100px;
+    width: 100px;
+    margin-right: 50px;
+  }
+`;
+
+const BioText = styled.div`
+  color: white;
+
+  display: grid;
+  grid-template-rows: repeat(4);
+  align-items: center;
+
+  h1 {
+    font-weight: bold;
+  }
+  ul {
+    display: grid;
+    grid-template-columns: 0.7fr 1fr 1fr;
+    max-width: 400px;
+    span {
+      font-weight: bold;
+    }
+  }
+`;
 
 interface BioDataTypeObject {
   profile_image_url: string;
@@ -30,5 +67,36 @@ export default function ProfileHeader() {
     fetchData();
   }, []);
 
-  return <>{bioData ? <p>bioData[0].bio</p> : <p>no data</p>}</>;
+  return (
+    <>
+      {bioData ? (
+        <ProfileHeaderContainer>
+          <ProfileImage
+            src={bioData[0].profile_image_url}
+            width={150}
+            height={150}
+            altText="Profile Image"
+          />
+          <BioText>
+            <h1>Nature Images</h1>
+            <ul>
+              <li>
+                <span>47</span> post
+              </li>
+              <li>
+                <span>2,313</span> followers
+              </li>
+              <li>
+                <span>2,446</span> following
+              </li>
+            </ul>
+            <h1>{bioData[0].name} </h1>
+            <p>{bioData[0].bio}</p>
+          </BioText>
+        </ProfileHeaderContainer>
+      ) : (
+        <p>no data</p>
+      )}
+    </>
+  );
 }
